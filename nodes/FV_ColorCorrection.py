d76e3d6d2dc9c2acb9b45c539aeab9d7f3ac7f37
Fictiverse <111762798+Fictiverse@users.noreply.github.com>
2023-10-29 21:25:23 +0100

    Add files via upload

--- nodes/FV_ColorCorrection.py
+++ nodes/FV_ColorCorrection.py
@@ -43,6 +43,6 @@ class ColorCorrection:
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "Color correction": ColorCorrect
+    "Color correction": ColorCorrection
 }
 
