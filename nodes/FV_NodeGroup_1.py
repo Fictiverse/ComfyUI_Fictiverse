8f34eea93412518b262027c32c3d948aab91e421
Fictiverse <111762798+Fictiverse@users.noreply.github.com>
2023-11-20 04:00:57 +0100

    Add files via upload

--- nodes/FV_NodeGroup_1.py
+++ nodes/FV_NodeGroup_1.py
@@ -4,6 +4,8 @@ from skimage.exposure import match_histograms
 from PIL import Image
 from enum import Enum
 import torch
+import torch.nn.functional as F
+from torchvision import transforms
 
 # PIL to Tensor
 def pil2tensor(image):
@@ -99,13 +101,104 @@ class Displace_Image: #Modified version of WAS node : https://github.com/WASasqu
                 disp = tensor2pil(displacement_maps[i])
             else:
                 disp = tensor2pil(displacement_maps[-1])
-            disp = self.resize_and_crop(disp, img.size)
-            displaced_images.append(pil2tensor(Tools.displace_image(img, disp, amplitudeX, amplitudeY)))
+            disp = Tools.resize_and_crop(disp, img.size)
+            displaced_images.append(pil2tensor(Tools.displace_imageNP(img, disp, amplitudeX, amplitudeY)))
 
         displaced_images = torch.cat(displaced_images, dim=0)
 
-        return (displaced_images, )
+        return (displaced_images, )     
+ 
+
+
+
+
+
+class AddNoiseToImageWithMask: #Modified version of WAS node : https://github.com/WASasquatch/was-node-suite-comfyui
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "masks": ("IMAGE",),
+                "strength": ("FLOAT", {"default": 0.5, "min": 0, "max": 1, "step": 0.05}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("images",)
+    FUNCTION = "addNoiseToImageWithMask"
+    CATEGORY = "Fictiverse"
+
+    def addNoiseToImageWithMask(self, images, masks, strength):
+    
+        Tools = Tools_Class()
+
+        out_images = []
+        for i in range(len(images)):
+            img = tensor2pil(images[i])
+            if i < len(masks):
+                mask = tensor2pil(masks[i])
+            else:
+                mask = tensor2pil(masks[-1])
+            mask = Tools.resize_and_crop(mask, img.size)
+            out_images.append(pil2tensor(Tools.add_noise_with_mask(img, mask, strength)))
+
+        out_images = torch.cat(out_images, dim=0)
+
+        return (out_images, )
         
+     
+
+class DisplaceImageWithDepth: #Modified version of WAS node : https://github.com/WASasquatch/was-node-suite-comfyui
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "Image": ("IMAGE",),
+                "Depth": ("IMAGE",),
+                "X": ("INT", {"default": 0, "min": -4096, "max": 4096, "step": 1}),
+                "Y": ("INT", {"default": 0, "min": -4096, "max": 4096, "step": 1}),
+                "Zoom": ("FLOAT", {"default": 1.0, "min": 0.1, "max": 100, "step": 0.1}),
+                "LayerCount": ("INT", {"default": 8, "min": 2, "max": 255, "step": 1}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE","IMAGE",)
+    RETURN_NAMES = ("images", "combined",)
+    FUNCTION = "displaceImageWithDepth"
+    CATEGORY = "Fictiverse"
+
+    def displaceImageWithDepth(self, Image, Depth, X, Y, Zoom, LayerCount):
+    
+        Tools = Tools_Class()
+
+        result_images = []
+        img = tensor2pil(Image[0])
+        mask = tensor2pil(Depth[0])
+        mask = Tools.resize_and_crop(mask, img.size)
+        
+        layers, combined = Tools.apply_perspective_transformation(img, mask, X, Y, Zoom, LayerCount)
+        print("Number of images ::::::::", len(layers))
+
+        for layer in layers:
+            result_images.append(pil2tensor(layer))
+
+        result_images = torch.cat(result_images, dim=0)
+
+        #return (result_images, ) 
+        return (result_images, pil2tensor(combined))
+####################################################################
+
+
+
+class Tools_Class():
+    
     def resize_and_crop(self, image, target_size):
         width, height = image.size
         target_width, target_height = target_size
@@ -126,11 +219,7 @@ class Displace_Image: #Modified version of WAS node : https://github.com/WASasqu
         bottom = top + target_height
         image = image.crop((left, top, right, bottom))
 
-        return image        
- 
-
-
-class Tools_Class():
+        return image   
 
     def displace_image(self, image, displacement_map, amplitudeX, amplitudeY):
 
@@ -175,11 +264,285 @@ class Tools_Class():
                 result.putpixel((x, y), pixel)
 
         return result  
+
+
+    def displace_imageNP(self, image, displacement_map, amplitudeX, amplitudeY):
+        image = image.convert('RGB')
+        displacement_map = displacement_map.convert('L')
+
+        # Convert PIL images to NumPy arrays
+        image_arr = np.array(image)
+        displacement_arr = np.array(displacement_map)
+
+        height, width, _ = image_arr.shape
+        result_arr = np.zeros((height, width, 3), dtype=np.uint8)
+
+        # Calculate displacements
+        displacement_normalized = displacement_arr / 255.0
+        displacement_amountX = amplitudeX * displacement_normalized
+        displacement_amountY = amplitudeY * displacement_normalized
+
+        # Create grids of x and y coordinates
+        x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
+
+        # Calculate new coordinates
+        new_x = np.clip(x_coords + displacement_amountX, 0, width - 1).astype(int)
+        new_y = np.clip(y_coords + displacement_amountY, 0, height - 1).astype(int)
+
+        # Apply mirror reflection at edges and corners
+        new_x = np.where(new_x < 0, -new_x, new_x)
+        new_x = np.where(new_x >= width, 2 * width - new_x - 1, new_x)
+        new_y = np.where(new_y < 0, -new_y, new_y)
+        new_y = np.where(new_y >= height, 2 * height - new_y - 1, new_y)
+
+        # Fetch pixels from original image at new locations
+        result_arr = image_arr[new_y, new_x]
+
+        # Create PIL Image from NumPy array
+        result = Image.fromarray(result_arr)
+
+        return result
+
+
+
+
+
+
+
+
+
+
+
+    def displaceImageWithDepth(self, image, mask, amplitudeX, amplitudeY, amplitudeZ, layerCount):
+
+        image = image.convert('RGB')
+        mask = mask.convert('L')
+        width, height = image.size
+
+        layerStep = int(255/layerCount)
+        imageLayers = []
+        
+        for l in range(layerCount):
+        
+            layer = Image.new('RGBA', (width, height))
+            colorTarget = max(0, min(l*layerStep, 255))
+            colorRangeMin = max(0, min(colorTarget-layerStep, 255))
+            colorRangeMax = max(0, min(colorTarget+layerStep, 255))
+            colorRange = range(colorRangeMin, colorRangeMax, 1)
+          
+            for y in range(height):
+                for x in range(width):        
+                    maskValue = mask.getpixel((x, y))
+                    if maskValue in colorRange:
+                        
+                        amplitude = l/layerCount
+                        offsetX = int(amplitudeX*amplitude*amplitude)
+                        offsetY = int(amplitudeY*amplitude*amplitude)
+                        offsetZ = int(amplitudeZ*amplitude*amplitude)
+                        nX = x+offsetX
+                        nY = y+offsetY
+
+                        if nX >=0 and nX<width and nY >=0 and nY<height:
+                            layer.putpixel((nX, nY), image.getpixel((x, y)))  
+                            
+                        #layer.putpixel((x, y), image.getpixel((x, y)))    
+                        
+            imageLayers.append(layer)
+
+        return imageLayers  
+
+    def apply_perspective_transformation(self, image_pil, depth_map_pil, tx, ty, zoom, num_layers):
+        # Convert PIL images to NumPy arrays
+        image = np.array(image_pil)
+        depth_map = np.array(depth_map_pil)
+        parallax_factor = 1
+        if num_layers < 1:
+            raise ValueError("Le nombre de couches doit être supérieur ou égal à 1.")
+
+        # Créer un tableau vide pour stocker les couches
+        layers = []
+
+        # Calculer la plage de profondeur
+        min_depth = np.min(depth_map)
+        max_depth = np.max(depth_map)
+        depth_range = max_depth - min_depth
+        
+        # Create an alpha channel (example)
+        alpha_channel = np.full((image.shape[0], image.shape[1]), 255, dtype=np.uint8)
+        # Create an empty RGBA image with the combined dimensions
+        combined_image = np.dstack((image, alpha_channel))     
+        
+        # Get the size (width and height) of the target image
+        width, height = image_pil.size
+        imagesCombined = Image.new("RGBA", (width, height), (0, 0, 0, 0)) 
+        #imagesCombined.paste(image_pil, (0, 0))
+
+        # Créer les couches en fonction du nombre spécifié
+        for i in range(num_layers):
+            # Déterminer les valeurs de profondeur minimale et maximale pour cette couche
+            layer_min_depth = min_depth + (i / num_layers) * depth_range
+            layer_max_depth = min_depth + ((i + 1) / num_layers) * depth_range
+
+
+
+
+
+
+
+
+            # Combine the current merged_image with the imagesCombined
+            #imagesCombined = Image.alpha_composite(imagesCombined, Image.fromarray(merged_image))
+
+
+
+
+
+            # Sélectionner les pixels de la depth map qui appartiennent à cette couche
+            layer_mask = np.logical_and(depth_map >= layer_min_depth, depth_map <= layer_max_depth)
+            #layer_mask = np.logical_and(depth_map >= layer_min_depth, depth_map < layer_max_depth)
+            #layer_mask = np.logical_and(depth_map >= layer_min_depth, 0 < 1)
+            color_rgb = image[:, :, :3]
+            alpha_channel = (layer_mask[:, :, 0] * 255).astype(np.uint8)
+
+             # Create an alpha channel with a gradual transition for this layer
+            #alpha_channel = ((depth_map - layer_min_depth) / (layer_max_depth - layer_min_depth))
+            #alpha_channel = np.clip(alpha_channel, 0, 1)  # Ensure values are in the [0, 1] range
+            #alpha_channel = (alpha_channel[:, :, 0] * 255).astype(np.uint8)
+
+
+
+
+
+
+            # Create an RGBA image by stacking the RGB channels with the alpha channel
+            merged_image = np.dstack((color_rgb, alpha_channel))
+            
+            # Calculate the parallax_factor for this layer
+            parallax_factor = i / (num_layers - 1)
+            
+            # Calculate the translation for this layer
+            tx_offset = int(tx * parallax_factor)
+            ty_offset = int(ty * parallax_factor)
+            
+            # Determine the new dimensions of the translated image
+            new_height = merged_image.shape[0]
+            new_width = merged_image.shape[1]
+
+            # Create an empty image with the same shape as merged_image
+            translated_mask = np.zeros_like(merged_image)
+
+            # Calculate the cropping box
+            x1, x2 = max(0, -tx_offset), min(new_width, new_width - tx_offset)
+            y1, y2 = max(0, -ty_offset), min(new_height, new_height - ty_offset)
+
+            # Calculate the region to copy from the original image
+            src_x1, src_x2 = max(0, tx_offset), min(new_width, new_width + tx_offset)
+            src_y1, src_y2 = max(0, ty_offset), min(new_height, new_height + ty_offset)
+
+            # Copy the pixels from the original image to the translated image
+            translated_mask[y1:y2, x1:x2] = merged_image[src_y1:src_y2, src_x1:src_x2]
+            
+            z = i*zoom/num_layers + 1
+            translated_mask = self.cv2_clipped_zoom(translated_mask, z)
+            layer_image = Image.fromarray(translated_mask)
+            layers.append(layer_image)
+
+
+
+            # Replace visible pixels of image1 with corresponding pixels from image2
+            imagesCombined.paste(layer_image, (0, 0), layer_image)
+
+
+        return layers, imagesCombined
+ 
+    
+
+    def cv2_clipped_zoom(self, img, zoom_factor=0):
+
+        """
+        Center zoom in/out of the given image and returning an enlarged/shrinked view of 
+        the image without changing dimensions
+        ------
+        Args:
+            img : ndarray
+                Image array
+            zoom_factor : float
+                amount of zoom as a ratio [0 to Inf). Default 0.
+        ------
+        Returns:
+            result: ndarray
+               numpy ndarray of the same shape of the input img zoomed by the specified factor.          
+        """
+        if zoom_factor == 0:
+            return img
+
+
+        height, width = img.shape[:2] # It's also the final desired shape
+        new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
+    
+        ### Crop only the part that will remain in the result (more efficient)
+        # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
+        y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
+        y2, x2 = y1 + height, x1 + width
+        bbox = np.array([y1,x1,y2,x2])
+        # Map back to original image coordinates
+        bbox = (bbox / zoom_factor).astype(np.int)
+        y1, x1, y2, x2 = bbox
+        cropped_img = img[y1:y2, x1:x2]
+    
+        # Handle padding when downscaling
+        resize_height, resize_width = min(new_height, height), min(new_width, width)
+        pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) //2
+        pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
+        pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0,0)] * (img.ndim - 2)
+    
+        result = cv2.resize(cropped_img, (resize_width, resize_height))
+        result = np.pad(result, pad_spec, mode='constant')
+        assert result.shape[0] == height and result.shape[1] == width
+        return result
+
+
+
+
+
+
+
+
+
+    def clampPx(self, value):      
+        return max(0, min(value, 255))
+
+ 
+        
+        
+    def add_noise_with_mask(self, image, mask, strength):
+        # Convert the input image and mask to NumPy arrays
+        image_array = np.array(image)
+        mask_array = np.array(mask)
+
+        # Generate random noise with the same shape as the image
+        noise = np.random.normal(scale=strength, size=image_array.shape)
+
+        # Apply the mask to the noise
+        noise *= mask_array
+
+        # Add the noise to the image
+        noisy_image_array = image_array + noise
+
+        # Clip the pixel values to the valid range (0-255)
+        noisy_image_array = np.clip(noisy_image_array, 0, 255).astype(np.uint8)
+
+        # Convert the NumPy array back to an image
+        noisy_image = Image.fromarray(noisy_image_array)
+
+        return noisy_image
         
         
         
         
 NODE_CLASS_MAPPINGS = {
         "Color correction": Color_Correction,
-        "Displace Images with Mask": Displace_Image
+        "Displace Images with Mask": Displace_Image,
+        "Add Noise to Image with Mask": AddNoiseToImageWithMask,
+        "Displace Image with Depth": DisplaceImageWithDepth,
 }
