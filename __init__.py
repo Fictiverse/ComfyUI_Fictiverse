da41fed95cef681906f2658bb2c7bdbfa5d4c1b7
Fictiverse <111762798+Fictiverse@users.noreply.github.com>
2024-10-26 03:07:43 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
 import os
 import importlib
 
-
 NODE_CLASS_MAPPINGS = {}
 
- print("\033[38;5;229m Fictiverse nodes \033[0m")   
 for node in os.listdir(os.path.dirname(__file__) + os.sep + 'nodes'):
     if node.startswith('FV_'):
         node = node.split('.')[0]
         node_import = importlib.import_module('custom_nodes.ComfyUI_Fictiverse.nodes.' + node)
-        print("Imported : " + "\033[38;5;229m" + node + + "\033[0m")
+        #print("Imported : " + "\033[38;5;229m" + node + "\033[0m")
         NODE_CLASS_MAPPINGS.update(node_import.NODE_CLASS_MAPPINGS)
-
+print("\033[38;5;229m Fictiverse nodes loaded. \033[0m")  
 #WEB_DIRECTORY = "./web"
